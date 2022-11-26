829b55c18c19727da8bfd78a99ea7bcf5b6a61ec
Oleg Kizimenko <okizimenko@sun-strategy.com>
2022-11-26 19:08:28 +0200

    added rest of the templates

--- GentlmenBot/GentlmenBot/Answers.cs
+++ GentlmenBot/GentlmenBot/Answers.cs
@@ -6,6 +6,25 @@
         public static string What_are_the_courses_Amdaris_covers = "asp.net core, java, java, english cources";
         public static string What_is_the_percent_of_money_Amdaris_covers_for_certification = "50%";
         public static string What_is_the_percent_of_money_Amdaris_covers_for_cources = "50%";
-        public static string What_public_holidays_do_we_have = "";
+        public static string GetPublicHolidays(string region)
+        {
+            return region switch
+            {
+                "Ua" => PublicHolidays.Ua,
+                "Romania" => PublicHolidays.Romania,
+                "Moldova" => PublicHolidays.Moldova,
+                "Britain" => PublicHolidays.Britain,
+                "Bolgaria" => PublicHolidays.Bolgaria,
+                _ => "Unknown"
+            };
+
+            
+        }
+
+        public static string NeedToAttendToSomewhere = @"Tell your delivery lead ";
+        public static string NoticePeriodOfAnnualLeave = "2 weaks";
+        public static string What_expenses_are_covered_when_the_business_trips = @"Business (or corporate) travel insurance covers different risks an employee might face during a business trip. The primary categories of coverage include trip cancellation, medical expenses, evacuation, and luggage or other personal loss.";
+        public static string Where_can_I_get_a_branded_T_shirt_other_merch = @"Ask HR";
+        public static string Do_we_have_any_branded_notepadspensumbrella = @"";
     }
 }
