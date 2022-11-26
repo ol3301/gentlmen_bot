b21724efde8460a38747e28b1d2668dfab8dfe3e
Ruslan Tytarenko <rtytarenko@sun-strategy.com>
2022-11-26 19:53:49 +0200

    Upd answers

--- GentlmenBot/GentlmenBot/Answers.cs
+++ GentlmenBot/GentlmenBot/Answers.cs
@@ -24,11 +24,32 @@ namespace GentlmenBot
             Questions.Add(new Question("What type of certifications does Amadris cover?", 
                 "https://amdaris.sharepoint.com/:x:/r/_layouts/15/Doc.aspx?sourcedoc=%7BF2369EEE-9F5A-47C6-95D3-3A812B22C382%7D&file=Certifications%202020%20(5).xlsx&action=default&mobileredirect=true&DefaultItemOpen=1", 
                 "cover","certificate","type"));
+            Questions.Add(new Question("What are the courses Amdaris covers?",
+                "asp.net core, java, java, english cources",
+                ""));
+            Questions.Add(new Question("What is the % of money Amdaris coversfor certification?",
+                "50%",
+                ""));
+            Questions.Add(new Question("What is the % of money Amdaris cover for courses?",
+                "50%",
+                ""));
+            Questions.Add(new Question("I need to attend a funeral,whom should Iannounce about this? Doesthe business providesome additional days for this kind of event?",
+                "Tell your delivery lead",
+                ""));
+            Questions.Add(new Question("What is the notice period for annual leave?",
+                "2 weaks",
+                ""));
+            Questions.Add(new Question("What expenses are covered whenthe business trips?",
+                "Business (or corporate) travel insurance covers different risks an employee might face during a business trip. The primary categories of coverage include trip cancellation, medical expenses, evacuation, and luggage or other personal loss.",
+                ""));
+            Questions.Add(new Question("Where can Igeta branded T-shirt/ other merch?",
+                "Ask HR",
+                ""));
+            Questions.Add(new Question("Dowe have any branded notepads/pens/umbrella?",
+                "We have notepads, pens, umbrella",
+                ""));
         }
-        public static string What_type_of_certifications_does_Amadris_cover = "https://amdaris.sharepoint.com/:x:/r/_layouts/15/Doc.aspx?sourcedoc=%7BF2369EEE-9F5A-47C6-95D3-3A812B22C382%7D&file=Certifications%202020%20(5).xlsx&action=default&mobileredirect=true&DefaultItemOpen=1";
-        public static string What_are_the_courses_Amdaris_covers = "asp.net core, java, java, english cources";
-        public static string What_is_the_percent_of_money_Amdaris_covers_for_certification = "50%";
-        public static string What_is_the_percent_of_money_Amdaris_covers_for_cources = "50%";
+
         public static string GetPublicHolidays(string region)
         {
             return region switch
@@ -41,11 +62,5 @@ namespace GentlmenBot
                 _ => "Unknown"
             };
         }
-
-        public static string NeedToAttendToSomewhere = @"Tell your delivery lead ";
-        public static string NoticePeriodOfAnnualLeave = "2 weaks";
-        public static string What_expenses_are_covered_when_the_business_trips = @"Business (or corporate) travel insurance covers different risks an employee might face during a business trip. The primary categories of coverage include trip cancellation, medical expenses, evacuation, and luggage or other personal loss.";
-        public static string Where_can_I_get_a_branded_T_shirt_other_merch = @"Ask HR";
-        public static string Do_we_have_any_branded_notepadspensumbrella = @"";
     }
 }
