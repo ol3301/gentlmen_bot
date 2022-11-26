9e5e0bb1bbdafdd671387539c6d0a8046fef1a19
Oleg Kizimenko <okizimenko@sun-strategy.com>
2022-11-26 20:21:32 +0200

    add tags search

--- GentlmenBot/GentlmenBot/Answers.cs
+++ GentlmenBot/GentlmenBot/Answers.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace GentlmenBot
 {
@@ -14,6 +15,11 @@ namespace GentlmenBot
             Answer = answer;
             Tags = tags;    
         }
+
+        public bool CompareTags(string[] tags)
+        {
+            return Tags.Where(x => tags.Where(x1 => StringUtils.CalculateSimilarity(x, x1) > 0.70).ToList().Count > 0).Any();
+        }
     }
     public static class Answers
     {
@@ -26,28 +32,35 @@ namespace GentlmenBot
                 "cover","certificate","type"));
             Questions.Add(new Question("What are the courses Amdaris covers?",
                 "asp.net core, java, java, english cources",
-                ""));
-            Questions.Add(new Question("What is the % of money Amdaris coversfor certification?",
+                "course", "cover", "amdaris"));
+            Questions.Add(new Question("What is the % of money Amdaris covers for certification?",
                 "50%",
-                ""));
+                "cover", "money", "certification", "%"));
             Questions.Add(new Question("What is the % of money Amdaris cover for courses?",
                 "50%",
-                ""));
+                "course", "cover"));
             Questions.Add(new Question("I need to attend a funeral,whom should Iannounce about this? Doesthe business providesome additional days for this kind of event?",
                 "Tell your delivery lead",
-                ""));
+                "attend", "annual", "emergency", "business", "event"));
             Questions.Add(new Question("What is the notice period for annual leave?",
                 "2 weaks",
-                ""));
+                "period", "annual", "leave", "what"));
             Questions.Add(new Question("What expenses are covered whenthe business trips?",
                 "Business (or corporate) travel insurance covers different risks an employee might face during a business trip. The primary categories of coverage include trip cancellation, medical expenses, evacuation, and luggage or other personal loss.",
-                ""));
+                "business trips", "trips", "cover", "insurance", "risk", "employee", "face", ));
             Questions.Add(new Question("Where can Igeta branded T-shirt/ other merch?",
                 "Ask HR",
                 ""));
             Questions.Add(new Question("Dowe have any branded notepads/pens/umbrella?",
                 "We have notepads, pens, umbrella",
                 ""));
+        }
+
+        public static List<Question> FindAppropriateQuestion(string text)
+        {
+            var tags = text.Split(' ');
+
+            return Questions.Where(x => x.CompareTags(tags)).ToList();
         }
 
         public static string GetPublicHolidays(string region)
--- GentlmenBot/GentlmenBot/Bots/EchoBot.cs
+++ GentlmenBot/GentlmenBot/Bots/EchoBot.cs
@@ -6,6 +6,7 @@
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Schema;
 using System.Collections.Generic;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -15,8 +16,14 @@ namespace GentlmenBot.Bots
     {
         protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
         {
-            var replyText = $"Echo: {turnContext.Activity.Text}";
-            await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);
+            var questions = Answers.FindAppropriateQuestion(turnContext.Activity.Text);
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in questions)
+                sb.AppendLine($"Question: {item.Name} - Answer: {item.Answer}");
+            
+            
+            await turnContext.SendActivityAsync(MessageFactory.Text(sb.ToString(), sb.ToString()), cancellationToken);
         }
 
         protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
