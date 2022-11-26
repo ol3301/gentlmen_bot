6f3b6daa2ac313666650c1bbcd6439cb20aade2d
Ruslan Tytarenko <rtytarenko@sun-strategy.com>
2022-11-26 19:21:17 +0200

    Upd db1

--- GentlmenBot/GentlmenBot/Context/GentlemenContext.cs
+++ GentlmenBot/GentlmenBot/Context/GentlemenContext.cs
@@ -9,7 +9,6 @@ namespace GentlmenBot.Context
 
         public GentlemenContext()
         {
-            Database.EnsureCreated();
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
---
+++ GentlmenBot/GentlmenBot/Migrations/20221126172025_Update1.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GentlmenBot.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "HolidaysLeft",
+                table: "Users",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Region",
+                table: "Users",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HolidaysLeft",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Region",
+                table: "Users");
+        }
+    }
+}
