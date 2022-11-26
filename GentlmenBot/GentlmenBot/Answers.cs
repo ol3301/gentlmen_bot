cd42ea75cdbd7137672f04d47c3fd93211a16ab8
Oleg Kizimenko <okizimenko@sun-strategy.com>
2022-11-26 19:06:26 +0200

    added templates of questions

---
+++ GentlmenBot/GentlmenBot/Answers.cs
@@ -0,0 +1,11 @@
+﻿namespace GentlmenBot
+{
+    public static class Answers
+    {
+        public static string What_type_of_certifications_does_Amadris_cover = "https://amdaris.sharepoint.com/:x:/r/_layouts/15/Doc.aspx?sourcedoc=%7BF2369EEE-9F5A-47C6-95D3-3A812B22C382%7D&file=Certifications%202020%20(5).xlsx&action=default&mobileredirect=true&DefaultItemOpen=1";
+        public static string What_are_the_courses_Amdaris_covers = "asp.net core, java, java, english cources";
+        public static string What_is_the_percent_of_money_Amdaris_covers_for_certification = "50%";
+        public static string What_is_the_percent_of_money_Amdaris_covers_for_cources = "50%";
+        public static string What_public_holidays_do_we_have = "";
+    }
+}
---
+++ GentlmenBot/GentlmenBot/PublicHolidays.cs
@@ -0,0 +1,108 @@
+﻿namespace GentlmenBot
+{
+    public static class PublicHolidays
+    {
+        public static string Ua = @"
+7 Jan	Thu	Orthodox Christmas Day
+8 Mar	Mon	Women's Day
+1 May	Sat	Labor Day
+2 May	Sun	Orthodox Easter Sunday
+3 May	Mon	Orthodox Easter Monday
+3 May	Mon	Labor Day Holiday
+9 May	Sun	Victory Day over Nazism in World War II
+10 May	Mon	Victory Day over Nazism in World War II Holiday
+20 Jun	Sun	Orthodox Whit Sunday
+21 Jun	Mon	Orthodox Whit Monday
+28 Jun	Mon	Constitution Day
+24 Aug	Tue	Independence Day
+14 Oct	Thu	Defender's Day
+25 Dec	Sat	Catholic Christmas Day
+";
+
+        public static string Moldova = @"
+Jan 1	Saturday	New Year's Day (Anul Nou)	
+Jan 7	Friday	Orthodox Christmas (Craciunul pe stil vechi)	
+Jan 8	Saturday	Orthodox Christmas Holiday	
+Mar 8	Tuesday	International Women's Day	
+Apr 24	Sunday	Orthodox Easter (Paste)	
+Apr 25	Monday	Orthodox Easter Monday (Doua zi de Paste)	
+May 1	Sunday	International Solidarity Day of Workers	
+May 2	Monday	Memory/Parents' Day (Pastele Blajinilor)	
+May 9	Monday	Victory Day / Day of Europe	
+June 1	Wednesday	International Children's Day	
+Aug 27	Saturday	Independence Day (Ziua Republicii)	
+Aug 31	Wednesday	National Language Day (Limba Noastra)	
+Oct 14	Friday	Chisinau Day (Hramul Chisinaului)	municipal
+Dec 25	Sunday	Western Christmas (Craciunul pe stil nou)
+";
+
+        public static string Romania = @"
+Jan 1	Saturday	New Year's Day (Anul nou)	
+Jan 2	Sunday	Second Day of the New Year	
+Jan 24	Monday	Union of the Romanian Principalities (Ziua Micii Uniri)	
+Apr 22	Friday	Orthodox Good Friday (Vinerea Mare)	
+Apr 24	Sunday	Orthodox Easter (Pastele)	
+Apr 25	Monday	Orthodox Easter Monday (Doua zi de Pasti)	
+May 1	Sunday	Labour Day (Ziua muncii)	
+June 1	Wednesday	International Children's Day	
+June 12	Sunday	Pentecost (Rusalii)	
+June 13	Monday	Pentecost Monday (Doua zi de Rusalii)	
+Aug 15	Monday	Dormition of the Virgin Mary (Adormirea Maicii Domnului)	
+Nov 30	Wednesday	Feast of Saint Andrew (Sfântul Andrei)	
+Dec 1	Thursday	National Day (Ziua unirii)	
+Dec 2	Friday	Public Sector Holiday	government
+Dec 25	Sunday	Christmas Day (Craciunul)	
+Dec 26	Monday	Second Day of Christmas
+";
+
+        public static string Poland = @"
+1 Jan	Sat	New Year's Day
+6 Jan	Thu	Epiphany
+17 Apr	Sun	Easter Sunday
+18 Apr	Mon	Easter Monday
+1 May	Sun	Labour Day
+3 May	Tue	Constitution Day
+5 Jun	Sun	Whit Sunday
+16 Jun	Thu	Corpus Christi
+15 Aug	Mon	Assumption Day
+1 Nov	Tue	All Saints' Day
+11 Nov	Fri	Independence Day
+25 Dec	Sun	Christmas Day
+26 Dec	Mon	2nd Day of Christmas
+";
+
+        public static string Britain = @"
+New Year's Day	Monday 3 January (substitute day)	Monday 2 January (substitute day)
+Good Friday	Friday 15 April	Friday 7 April
+Easter Monday	Monday 18 April	Monday 10 April
+Early May bank holiday	Monday 2 May	Monday 1 May
+Coronation of King Charles III 	 	Monday 8 May
+Spring bank holiday	Thursday 2 June	Monday 29 May
+Platinum Jubilee bank holiday	Friday 3 June	
+Summer bank holiday	Monday 29 August	Monday 28 August
+State Funeral of Queen Elizabeth II	Monday 19 September	
+Christmas Day	Tuesday 27 December (substitute day)	Monday 25 December 
+Boxing Day	Monday 26 December	Tuesday 26 December
+";
+
+        public static string Bolgaria = @"
+1 Jan	Sat	New Year's Day
+3 Mar	Thu	Liberation Day
+22 Apr	Fri	Orthodox Good Friday
+23 Apr	Sat	Orthodox Easter Saturday
+24 Apr	Sun	Orthodox Easter Sunday
+25 Apr	Mon	Orthodox Easter Monday
+1 May	Sun	Labour Day
+2 May	Mon	Labour Day Holiday
+6 May	Fri	Saint George's Day / Army Day
+24 May	Tue	Culture and Literacy Day
+6 Sep	Tue	Unification Day
+22 Sep	Thu	Independence Day
+1 Nov	Tue	Day of the Bulgarian Enlighteners *
+24 Dec	Sat	Christmas Eve
+25 Dec	Sun	Christmas Day
+26 Dec	Mon	2nd Day of Christmas
+27 Dec	Tue	Christmas Holiday
+";
+    }
+}
